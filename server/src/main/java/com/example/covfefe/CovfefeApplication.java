37f088e4d2aea37b86d1b88e9791b5673670ef9b
unnamedsong <dudefaceoff@gmail.com>
2018-06-18 13:23:58 +0900

    Init Project

---
+++ server/src/main/java/com/example/covfefe/CovfefeApplication.java
@@ -0,0 +1,12 @@
+package com.example.covfefe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovfefeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovfefeApplication.class, args);
+	}
+}
